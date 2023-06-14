7d2c696af7cfcbb205e639d5fbceefb85c52285b
Sergey Kuran <kuran.serg@gmail.com>
2023-06-14 19:42:18 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
 
-const Filter = ({ value }) => {
+// Компонент для пошуку, фільтрації данних при введені у інпут
+const Filter = ({ filter, onChange }) => {
   return (
     <label>
       Find contact by name
-      <input type="text" value={value} />
+      <input type="text" value={filter} onChange={onChange} />
     </label>
   );
 };
 
 Filter.propTypes = {
-  filter: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default Filter;
